f86af5436aa42374eb81f481342275c38c8193ff
akiyl <akshatthapliyal2004@gmail.com>
2026-05-29 22:38:41 +0530

    next updated

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,7 @@ import type { NextConfig } from "next";
 // next.config.js
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config: any) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ["@svgr/webpack"],
-    });
-    return config;
-  },
+  turbopack: {},
 };
 
 module.exports = nextConfig;
